8ef71ccf2d2d9cb04ea6c0f6e3e74c11f479a799
reagan <reaganm746@gmail.com>
2023-10-07 15:02:42 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const inputField = document.querySelector("#new-task-description");
+    const taskDescription = inputField.value;
+
+    const taskElement = document.createElement("li");
+    taskElement.textContent = taskDescription;
+
+    taskList.appendChild(taskElement);
+
+    inputField.value = "";
+  });
+ 
+const priorityDropdown = document.querySelector("#priority-level");
+
+priorityDropdown.addEventListener("change", function () {
+  const selectedPriority = priorityDropdown.value;
+  taskElement.classList.remove("high-priority", "medium-priority", "low-priority");
+  taskElement.classList.add(`${selectedPriority}-priority`);
+});
+
 });
